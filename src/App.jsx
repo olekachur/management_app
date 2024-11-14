6e786dd77306befd11e87433333505a25635a3ef
Oleksandr Kachur <lexandrkachur@gmail.com>
2024-11-14 15:18:54 +0200

    implement projects base componetnts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
+import React, { useState } from "react";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoSelectedProject from "./components/NoSelectedProject";
+
 function App() {
+  const [projectSate, setProjectState] = useState({
+    slectedProjectId: undefined,
+    projects: [],
+  });
+  let content;
+
+  function handleAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        slectedProjectId: null,
+      };
+    })
+  }
+
+  function handleProject(projectData) {
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      }
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    }
+    )
+  }
+
+  console.log(projectSate);
+
+  if (projectSate.slectedProjectId === null) {
+    content = <NewProject onAddProject={handleProject} />
+  } else if (projectSate.slectedProjectId === undefined) {
+    content = <NoSelectedProject onAddProject={handleAddProject} />
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onAddProject={handleAddProject} />
+      {content}
+    </main>
   );
 }
 
